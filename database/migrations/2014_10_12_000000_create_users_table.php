d9252784459f780f7423bef8542fd7962d42ff0a
Bogdan Pristupa <bp94drm@yandex.ru>
2023-05-21 18:58:40 +0300

    created midels and migrations: Role(id, name), Category(id, parentId, name, description), Tag(id,name,description), Topic(id, title, categoryId->foreign->constrained(categories)), TopicTag(id, topicId(foreign), tagId(foreign))

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,6 +26,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->softDeletes();
         });
     }
 
---
+++ database/migrations/2023_05_21_152444_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parentId')->default(0);
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
